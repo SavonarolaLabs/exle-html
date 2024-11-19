85b2ba1802b245e28753cff26b42e2d4ae398ce9
c8e4 <c8e4@proton.me>
2024-11-19 19:11:24 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'src',
+  root: 'src', // Use 'src' as the root directory
+  base: '/exle-html/', // This must match your repository name
+  build: {
+    outDir: '../dist', // Output the build files to '../dist' relative to 'src'
+    emptyOutDir: true,
+  },
   server: {
-    open: true
-  }
+    open: true,
+  },
 });
