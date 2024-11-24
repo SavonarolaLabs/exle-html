554462bf397f93f5f8d8737bbe244f971d581f89
c8e4 <c8e4@proton.me>
2024-11-24 02:37:24 +0100

    Updates

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+import { setupNavbar } from '../components/navbar/Navbar.js';
 import { renderLoansPage } from './LoansRenderer.js';
 import { renderRepaymentsPage } from './RepaymentsRenderer.js';
 
@@ -5,7 +6,7 @@ import { renderRepaymentsPage } from './RepaymentsRenderer.js';
 const BASE_DIR = document.querySelector('base').getAttribute('href') || '/';
 
 const renderComponent = async (selector, path) => {
-  const fullPath = `${BASE_DIR}${path.replace(/^\//, '')}`; // Resolve relative path with BASE_DIR
+  const fullPath = `${BASE_DIR}${path.replace(/^\//, '')}`;
   const response = await fetch(fullPath).catch((error) => console.error(`Error loading ${fullPath}:`, error));
   if (response) document.querySelector(selector).innerHTML = await response.text();
 };
@@ -25,32 +26,10 @@ const loadPage = async () => {
   if (hash === '/repayments') renderRepaymentsPage();
 };
 
-const setupThemeToggle = () => {
-  const body = document.body;
-  const themeToggleButton = document.querySelector('#theme-toggle');
-
-  const applyTheme = (theme) => {
-    body.setAttribute('data-theme', theme);
-    if (themeToggleButton) themeToggleButton.textContent = theme === 'dark' ? '☀️' : '🌙';
-  };
-
-  const currentTheme = localStorage.getItem('theme') || 'dark';
-  applyTheme(currentTheme);
-
-  if (themeToggleButton) {
-    themeToggleButton.addEventListener('click', () => {
-      const newTheme = body.getAttribute('data-theme') === 'dark' ? 'light' : 'dark';
-      localStorage.setItem('theme', newTheme);
-      applyTheme(newTheme);
-    });
-  }
-};
-
 document.addEventListener('DOMContentLoaded', async () => {
-  await renderComponent('#navbar', `components/Navbar.html`);
+  await setupNavbar(BASE_DIR); // Setup the navbar
   await loadPage();
-  await renderComponent('#footer', `components/Footer.html`);
-  setupThemeToggle();
+  await renderComponent('#footer', `${BASE_DIR}components/Footer.html`);
 });
 
 window.addEventListener('hashchange', loadPage);
